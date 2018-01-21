299eb323762a24310304717ad7756ac48c7b94fa
jonmoldover <jonmoldover@gmail.com>
2018-01-20 19:51:19 -0800

    comment refactor for LogFormatter.  Moved this inside of Logger.  Also removed un-used ContestRankings class

--- src/com/rcv/Logger.java
+++ src/com/rcv/Logger.java
@@ -2,15 +2,18 @@
  * Created by Jonathan Moldover on 7/8/17
  * Copyright 2018 Bright Spots
  * Purpose: Wrapper for console and file logging functions
- * All logging and audit output should use these methods
+ * All logging and audit output should use these methods to ensure output goes into audit file 
  * Version: 1.0
  */
 
 package com.rcv;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
 
 public class Logger {
 
@@ -45,7 +48,30 @@ public class Logger {
     sLogger.setUseParentHandlers(false);
   }
 
-  // single logger object this class wraps
+  // custom log formatter for log output string formatting
+  // extends the default logging Formatter class
+  private static class LogFormatter extends Formatter {
+
+    // overrides the format function with our custom formatter
+    // record: the LogRecord to be formatted into a string for output
+    // returns: the formatted string for output
+    @Override
+    public String format(LogRecord record) {
+      // builder to create the formatted log output string
+      StringBuilder stringBuilder = new StringBuilder();
+      // populate the string with output data
+      stringBuilder.append(new Date(record.getMillis()))
+          .append(" ")
+          .append(record.getLevel().getLocalizedName())
+          .append(": ")
+          .append(formatMessage(record))
+          .append(System.getProperty("line.separator"));
+
+      return stringBuilder.toString();
+    }
+  }
+
+  // the logger object this class wraps
   private final static java.util.logging.Logger sLogger = java.util.logging.Logger.getLogger("RCV");
 
 }
