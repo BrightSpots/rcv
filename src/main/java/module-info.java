ef5ebae44c2aa74c826fd65717a4ff62046e7f4e
jonmoldover <jonmoldover@gmail.com>
2020-07-10 10:53:53 -0400

    more cleanup for PR #458

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,13 @@ module network.brightspots.rcv {
   requires com.fasterxml.jackson.annotation;
   requires com.fasterxml.jackson.core;
   requires com.fasterxml.jackson.databind;
+  requires com.fasterxml.jackson.dataformat.xml;
   requires commons.csv;
   requires java.logging;
   requires javafx.controls;
   requires javafx.fxml;
   requires java.xml;
   requires poi.ooxml;
-  requires com.fasterxml.jackson.dataformat.xml;
   // enable reflexive calls from network.brightspots.rcv into javafx.fxml
   opens network.brightspots.rcv;
   // our main module
