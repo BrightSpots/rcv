cb6f45248394ed93cd31d7c8617c87fca3a29615
Jon Moldover <jonmoldover@gmail.com>
2022-06-09 10:46:36 -0700

    Allow percent % in path and file names #561 (#601)
    * Allow percent % in path and file names #561
    
    * fix line > 100 chars
    
    * fix tab spacing
    
    * fix imports
    
    * fix tabulation logging file pattern
    
    * Fix line > 100

--- src/main/java/network/brightspots/rcv/Logger.java
+++ src/main/java/network/brightspots/rcv/Logger.java
@@ -98,14 +98,24 @@ class Logger {
   }
 
   // adds file logging for a tabulation run
-  static void addTabulationFileLogging(String outputPath) throws IOException {
-    // use Level.FINE to capture audit info
-    tabulationHandler =
-        new FileHandler(outputPath, LOG_FILE_MAX_SIZE_BYTES, TABULATION_LOG_FILE_COUNT, true);
+  static void addTabulationFileLogging(String outputFolder, String timestampString)
+      throws IOException {
+    // log file name is: outputFolder + timestamp + log index
+    // FileHandler requires % to be encoded as %%.  %g is the log index
+    String tabulationLogPattern =
+            Paths.get(outputFolder.replace("%", "%%"),
+                    String.format("%s_audit_%%g.log", timestampString))
+                    .toAbsolutePath()
+                    .toString();
+
+    tabulationHandler = new FileHandler(tabulationLogPattern,
+            LOG_FILE_MAX_SIZE_BYTES,
+            TABULATION_LOG_FILE_COUNT,
+            true);
     tabulationHandler.setFormatter(formatter);
     tabulationHandler.setLevel(Level.FINE);
     logger.addHandler(tabulationHandler);
-    info("Tabulation logging to: %s", outputPath.replace("%g", "*"));
+    info("Tabulation logging to: %s", tabulationLogPattern.replace("%g", "0"));
   }
 
   // remove file logging once a tabulation run is completed
@@ -116,19 +126,24 @@ class Logger {
   }
 
   static void fine(String message, Object... obj) {
-    logger.log(Level.FINE, String.format(message, obj));
+    log(Level.FINE, message, obj);
   }
 
   static void info(String message, Object... obj) {
-    logger.log(Level.INFO, String.format(message, obj));
+    log(Level.INFO, message, obj);
   }
 
   static void warning(String message, Object... obj) {
-    logger.log(Level.WARNING, String.format(message, obj));
+    log(Level.WARNING, message, obj);
   }
 
   static void severe(String message, Object... obj) {
-    logger.log(Level.SEVERE, String.format(message, obj));
+    log(Level.SEVERE, message, obj);
+  }
+
+  private static void log(Level level, String message, Object... obj) {
+    // only call format if there are format args provided
+    logger.log(level, obj.length > 0 ? String.format(message, obj) : message);
   }
 
   // add logging to the provided text area for display to user in the GUI
