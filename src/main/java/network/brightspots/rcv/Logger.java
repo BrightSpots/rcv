18e3db76646d83675e7d8bb980fb4f4f46727d7f
Armin Samii <armin.samii@gmail.com>
2024-05-29 14:40:44 -0400

    remove unused import

--- src/main/java/network/brightspots/rcv/Logger.java
+++ src/main/java/network/brightspots/rcv/Logger.java
@@ -39,7 +39,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.BlockingDeque;
 import java.util.logging.FileHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
