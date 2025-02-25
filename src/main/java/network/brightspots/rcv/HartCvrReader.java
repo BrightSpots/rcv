a2e54414ed80832b6d27d9860ee3d5d1f39b5477
yezr <8996546+yezr@users.noreply.github.com>
2025-02-25 11:12:23 -0500

    clean up if/else when checking for candidate.code + candidate.name uniqueness

--- src/main/java/network/brightspots/rcv/HartCvrReader.java
+++ src/main/java/network/brightspots/rcv/HartCvrReader.java
@@ -121,23 +121,24 @@ class HartCvrReader extends BaseCvrReader {
             if (candidate.Code.equals(source.getUndeclaredWriteInLabel())) {
               candidate.Code = Tabulator.UNDECLARED_WRITE_IN_OUTPUT_LABEL;
             }
-            if (this.candidateCodesToCandidates.containsKey(candidate.Code)) {
-              if (!this.candidateCodesToCandidates.get(candidate.Code).Name.equals(candidate.Name)) {
-                String message = """
-                        Candidate Code %s associated with more than one candidate name.
-                        Originally associated with name '%s'.
-                        In CVR at '%s' it is associated with '%s'.
-                        """.formatted(candidate.Code,
+            Candidate existingCandidate = this.candidateCodesToCandidates.get(candidate.Code);
+            if (existingCandidate != null &&
+                    !this.candidateCodesToCandidates.get(candidate.Code).Name.equals(candidate.Name)) {
+                String message =
+                        "Candidate Code %s associated with more than one candidate name." +
+                        "Originally associated with name '%s'." +
+                        "In CVR at '%s' it is associated with '%s'."
+                        .formatted(candidate.Code,
                         this.candidateCodesToCandidates.get(candidate.Code).Name,
                         path.getFileName(), candidate.Name);
                 Logger.severe(message);
                 throw new CastVoteRecord.CvrParseException();
-              }
-            } else {
-              if (!candidate.Code.equals(Tabulator.UNDECLARED_WRITE_IN_OUTPUT_LABEL)) {
+            }
+
+            if (existingCandidate == null &&
+                    !candidate.Code.equals(Tabulator.UNDECLARED_WRITE_IN_OUTPUT_LABEL)) {
                 this.candidateCodesToCandidates.put(candidate.Code,
                         candidate);
-              }
             }
 
 
